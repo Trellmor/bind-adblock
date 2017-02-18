5a0126384653a6df403c723d049cd90e1a89522d
Daniel Triendl <daniel@pew.cc>
2017-02-18 18:29:16 +0100

    Initial import

---
+++ README.md
@@ -0,0 +1,50 @@
+# BIND ad blocker
+
+Fetch various blocklists and generate a BIND zone from them.
+
+Configure BIND to return `NXDOMAIN` for ad and tracking domains to stop clients from contacting them.
+
+Requires BIND 9.8 or newer for [RPZ](https://en.wikipedia.org/wiki/Response_policy_zone) support.
+
+Uses the following sources:
+
+* [Peter Lowe’s Ad and tracking server list](https://pgl.yoyo.org/adservers/)
+* [Malware domains](http://www.malwaredomains.com/)
+* [MVPS HOSTS](http://winhelp2002.mvps.org/)
+* [Adaway default blocklist](https://adaway.org/hosts.txt)
+* [hpHosts Ad and Tracking servers only](https://hosts-file.net/)
+
+## Setup
+
+### Configure BIND
+
+Add the `response-policy` statement to the BIND options
+
+```
+// For AdBlock
+response-policy {
+	zone "rpz.example.com";
+};
+```
+
+Add your rpz zone. Replace example.com with a domain of your choice.
+
+```
+// AdBlock
+zone "rpz.example.com" {
+	type master;
+	file "/etc/bind/db.rpz.example.com";
+};
+```
+
+## Usage
+
+    update-zonefile.py zonefile origin
+
+* zonefile: Path to the zone file to update
+* origin: Zone origin to use
+
+Example: `update-zonefile.py /etc/bind/db.rpz.example.com rpz.example.com`
+
+`update-zonefile.py` will update the zone file with the fetched adserver lists and issue a `rndc reload origin` afterwards.
+
---
+++ update-zonefile.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+
+'''
+Copyright (c) 2017 Daniel Triendl <daniel@pew.cc>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+import requests
+from pathlib import Path
+from datetime import datetime
+import email.utils as eut
+import os
+import hashlib
+import re
+import sys
+import dns.zone
+import subprocess
+
+lists = [
+    {'url': 'https://pgl.yoyo.org/as/serverlist.php?hostformat=nohtml&showintro=0'},
+    {'url': 'http://mirror1.malwaredomains.com/files/justdomains'},
+    {'url': 'http://winhelp2002.mvps.org/hosts.txt', 'regex': '^0\.0\\.0\\.0\\s+(?P<domain>([a-z0-9\\-_]+\\.)+[a-z]+)$'},
+    {'url': 'https://adaway.org/hosts.txt', 'regex': '^127\\.0\\.0\\.1 (?P<domain>([a-z0-9\\-_]+\\.)+[a-z]+)$'},
+    {'url': 'https://hosts-file.net/ad_servers.txt', 'regex': '^127\\.0\\.0\\.1\\s+(?P<domain>([a-z0-9\\-]+\\.)+[a-z]+)$'}
+]
+
+def download_list(url):
+    headers = None;
+
+    cache = Path('.cache', 'bind_adblock')
+    if not cache.is_dir():
+        cache.mkdir(parents=True)
+    cache = Path(cache, hashlib.sha1(url.encode()).hexdigest())
+
+    if cache.is_file():
+        last_modified = datetime.utcfromtimestamp(cache.stat().st_mtime)
+        headers = {'If-modified-since': eut.format_datetime(last_modified)}
+
+    r = requests.get(url, headers=headers)
+
+    if r.status_code == 200:
+        with cache.open('w') as f:
+            f.write(r.text)
+            
+        if 'last-modified' in r.headers:
+            last_modified = eut.parsedate_to_datetime(r.headers['last-modified']).timestamp()
+            os.utime(str(cache), times=(last_modified, last_modified))
+
+        return r.text
+
+    if cache.is_file():
+        with cache.open() as f:
+            return f.read()
+
+def parse_lists():
+    domains = set()
+    for l in lists:
+        data = download_list(l['url'])
+        if data:
+            print(l["url"])
+
+            lines = data.splitlines()
+            print("\t{} lines".format(len(lines)))
+
+            c = len(domains)
+
+            for line in data.splitlines():
+                if 'regex' in l:
+                    m = re.match(l['regex'], line)
+                    if m:
+                        domains.add(m.group('domain'))
+                else:
+                    domains.add(line)
+
+            print("\t{} domains".format(len(domains) - c))
+
+    print("\nTotal\n\t{} domains".format(len(domains)))
+    return domains;
+
+def load_zone(zonefile, origin):
+    zone_text = ''
+    with Path(zonefile).open('r') as f:
+        for line in f:
+            if "CNAME" in line:
+                break
+            zone_text += line
+    return dns.zone.from_text(zone_text, origin)
+
+def update_serial(zone):
+    soa = zone.get_rdataset('@', dns.rdatatype.SOA)[0]
+    soa.serial += 1
+
+def reload_zone(origin):
+    cmd = ['rndc', 'reload', origin]
+    r = subprocess.call(cmd)
+    if r != 0:
+        raise Exception('rndc failed with return code {}'.format(r))
+
+def usage(code=0):
+    print('Usage: update-zonefile.py zonefile origin');
+    exit(code);
+
+if len(sys.argv) != 3:
+    usage(1)
+
+zonefile = sys.argv[1]
+origin = sys.argv[2]
+
+zone = load_zone(zonefile, origin)
+update_serial(zone)
+
+domains = parse_lists()
+
+zone.to_file(zonefile)
+
+with Path(zonefile).open('a') as f:
+    for d in (sorted(domains)):
+        f.write(d + ' IN CNAME .\n')
+        f.write('*.' + d + ' IN CNAME .\n')
+
+reload_zone(origin)
