64af45109e2b2451e87af462dec12bc1df6e089c
Daniel Triendl <daniel@pew.cc>
2018-01-13 12:02:23 +0100

    Added License file

--- update-zonefile.py
+++ update-zonefile.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 '''
-Copyright (c) 2017 Daniel Triendl <daniel@pew.cc>
+Copyright (c) 2018 Daniel Triendl <daniel@pew.cc>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
